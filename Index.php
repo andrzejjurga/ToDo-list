01ede72b1b9ea92236760b09cac9cb544785d3cc
Andrzej Jurga <ajurga1025@gmail.com>
2021-01-23 22:40:31 +0100

    adding bootstrap

--- Index.php
+++ Index.php
@@ -4,10 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>ToDo</title>
 </head>
 <body>
-    
+
+<div class="alert alert-success text-center">  
+    <h1>Work in progress</h1>
+</div>
+
+<nav class="navbar bg-light navbar-light">
+    <a class="navbar-brand" href="#"> ToDo </a>
+    <a class="align-left" href="#"> Login </a>
+</nav>
+<div class="jumbotrone px-5">
+    <h1 class="display-4">Welcome back!</h1>
+    <p class="lead">Let's do something together</p>
+</div>
+
+
+
 
 
 
